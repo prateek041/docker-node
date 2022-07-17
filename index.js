95bf6e62efbc0285a4125d49b42dafeb8a37415a
prateek041 <prateeksingh9741@gmail.com>
2022-07-17 11:26:27 +0530

    connected to the database using DNS

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect("mongodb://prateek:mypass@mongoDB:27017/?authSource=admin"
+).then(() => {
+    console.log("connected to the database")
+}).catch((error) => {
+    console.log(error)
+})
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hello there this is me hehe</h2>")
+    res.send("<h2>Hello there this is me hehe hello this is me. this is the boobie girl</h2>")
 })
 
 app.listen(port, () => {
